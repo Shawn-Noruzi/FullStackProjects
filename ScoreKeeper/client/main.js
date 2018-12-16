71e269b7e257bfdea4b418c65ff426a180467868
shayan noruzi <shawn.noruzi@gmail.com>
2018-12-16 01:38:47 -0800

    Removal of static entries, new mongo db functionality, minimongo and mongo sync with new entries.

--- ScoreKeeper/client/main.js
+++ ScoreKeeper/client/main.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import { Players } from './../imports/api/players';
+import { Tracker } from 'meteor/tracker';
 
-const players = [{
-    _id: '1',
-    name: 'Shawn',
-    score: 99
-    },
-    {
-        _id: '2',
-        name: 'Brad',
-        score: -12
-    },
-    {
-        _id: '3',
-        name: 'Alex',
-        score: -1
-    }];
+
+
+
 const renderPlayers = function (playersList) {
-    return players.map(function (player) {
+    return playersList.map(function (player) {
         return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
     });
 };
 Meteor.startup(function () {
-    let title = "Score Keep";
-    let name = "shawn";
-    let jsx = (
-        <div>
-            <h1> {title} </h1>
-            <p>hello {name} </p>
-            <p>This is my 2nd P</p>
-            {renderPlayers(players)}
-        </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    Tracker.autorun(function () {
+        let players = Players.find().fetch();
+        let title = "Score Keep";
+        let name = "shawn";
+        let jsx = (
+            <div>
+                <h1> {title} </h1>
+                <p>hello {name} </p>
+                <p>This is my 2nd P</p>
+                {renderPlayers(players)}
+            </div>
+        );
+        ReactDOM.render(jsx, document.getElementById('app'));
+    });
+
+    Players.insert({
+        name: 'rad',
+        score: 45
+    });
+
 });
+
