fb4dc56389db890f456e78554a12250a083d1c3b
shawnSFU <snoruzi@sfu.ca>
2018-12-23 15:42:58 -0800

    React Components added

--- ScoreKeeper/client/main.js
+++ ScoreKeeper/client/main.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-import { Players } from './../imports/api/players';
 import { Tracker } from 'meteor/tracker';
-
+import { Players } from './../imports/api/players';
+import TitleBar from './../imports/UI/TitleBar';
 
 
 
 const renderPlayers = (playersList) => {
-    returnplayersList.map((player) => {
+    return playersList.map((player) => {
         return (
             <p key={player._id}>
                 {player.name} has {player.score} point(s).
 
-                <button onClick={() => {Players.update({ _id: player._id },{$inc { score, 1}});}}>+1 </button>;
-                <button onClick={() => {Players.update({ _id: player._id },{$inc { score, -1}});}}>-1 </button>;
-                <button onClick={() => Players.remove({ id: player._id })}>X</button>
+                
+                <button onClick={() => {Players.update(player._id, { $inc: { score: 1}});}}>+1</button>
+
+                <button onClick={() => {Players.update(player._id, { $inc: { score: -1 } });}}>-1</button>
+
+                <button onClick={() => Players.remove(player._id)}>X</button>
             </p>
         );
     });
@@ -34,19 +37,17 @@ const handleSubmit = (e) => {
             name: playerName,
             score: 0
         });
-    };
+    }
 };
+
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
         let players = Players.find().fetch();
         let title = "Score Keep";
-        let name = "shawn";
         let jsx = (
             <div>
-                <h1> {title} </h1>
-                <p>hello {name} </p>
-                <p>This is my 2nd P</p>
+                <TitleBar/>
                 {renderPlayers(players)}
             
                 <form onSubmit={handleSubmit}>
--- ScoreKeeper/server/main.js
+++ ScoreKeeper/server/main.js
@@ -2,6 +2,58 @@ import { Meteor } from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 
 Meteor.startup(() => {
+    class Person {
+        constructor(name = 'Anonymous', age = 0) {
+            this.name = name;
+            this.age = age;
+        }
+        getGreeting() {
+            return `Hi I'm ${this.name}.`;
+        }
+        getPersonDescription() {
+            return `${this.name} is ${this.age} old.`;
+        }
+    }
 
+
+    class Employee extends Person {
+        constructor(name,age,title) {
+            super(name, age);
+            this.title = title;
+        }
+        hasJob() {
+            return !!this.title;
+        }
+
+        getGreeting() {
+
+            if (this.title) {
+                return `hi i am ${this.name} i work as ${this.title}.`;
+            }
+            else {
+                return super.getGreeting();
+            }
+        }
+    }
+
+    class programmer extends Person {
+        constructor(name, age, language = 'assembly') {
+            super(name, age);
+            this.language = language;
+        }
+
+        getGreeting() {
+            if (this.language) {
+                return `hi i am ${this.name} i work as ${this.title} i speak ${this.language}.`;
+            }
+            else {
+                return super.getGreeting();
+            }
+        }
+    }
+
+    let person = new programmer('alex', 25, 'coder');
+
+    console.log(person.getGreeting());
 });
 
