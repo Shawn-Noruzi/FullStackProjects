8818e4d925c0b422477cc34dee884581abea700d
shawnSFU <snoruzi@sfu.ca>
2018-12-29 18:04:53 -0800

    Completed SCSS modifications. Completed Score Keep Application.

--- ScoreKeeper/client/main.js
+++ ScoreKeeper/client/main.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
-import { Players } from './../imports/api/players';
+import { Players, calculatePlayerPositions } from './../imports/api/players';
 import App from './../imports/UI/App';
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
         //sort the scores by value 
         let players = Players.find({}, { sort: { score: -1 } }).fetch();
+        let positionedPlayers = calculatePlayerPositions(players);
         let title = "Score Keep";
         //send props to App component, render to html page using ReactDOM
-        ReactDOM.render(<App title={title} players={players} />, document.getElementById('app'));
+        ReactDOM.render(<App title={title} players={positionedPlayers} />, document.getElementById('app'));
     });
 });
 
--- ScoreKeeper/imports/UI/Player.js
+++ ScoreKeeper/imports/UI/Player.js
@@ -4,12 +4,13 @@ import { Players } from './../api/players';
 
 export default class Player extends React.Component {
     render() {
+        let itemClassName = `item item--position-${this.props.player.rank}`;
         return (
-            <div key={this.props.player._id} className="item">
+            <div key={this.props.player._id} className={itemClassName}>
                 <div className="player">
                     <div>
                         <h3 className="player__name">{this.props.player.name}</h3>
-                        <p className="player__stats">{this.props.player.score} point(s).</p>
+                        <p className="player__stats">{this.props.player.position} Place - {this.props.player.score} point(s).</p>
                     </div>
                     <div className="player__actions">
                         <button className="button button--round" onClick={() => { Players.update(this.props.player._id, { $inc: { score: 1 } }); }}>+1</button>
